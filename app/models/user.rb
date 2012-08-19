4ef6d6c048943a2839c0000938448fb0879bda25
Stephen Wrathall <stevietw@gmail.com>
2012-08-19 22:13:05 +0800

    Add user microposts!

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
+  has_many :microposts, dependent: :destroy
 
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
@@ -27,6 +28,10 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
 
   private
 
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
     sequence(:name)  { |n| "Person #{n}" }
-    sequence(:email) { |n| "person_#{n}@example.com"}
+    sequence(:email) { |n| "person_#{n}@example.com"}   
     password "foobar"
     password_confirmation "foobar"
 
@@ -9,6 +9,10 @@ FactoryGirl.define do
       admin true
     end
   end
-end
 
+  factory :micropost do
+    content "Lorem ipsum"
+    user
+  end
+end
 
